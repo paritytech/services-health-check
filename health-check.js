f2bd55ad07789648ce49b4a57daca7ebe506e6bc
Jannis R <mail@jannisr.de>
2017-01-23 17:43:01 +0100

    basic health checks

--- health-check.js
+++ health-check.js
@@ -3,7 +3,68 @@
 const test = require('tape')
 const fetch = require('node-fetch')
 
-test('todo', (t) => {
+test('lookup service – mainnet', (t) => {
   t.plan(1)
-  t.pass('todo')
+  fetch('https://id.parity.io:443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
+})
+
+test('lookup service – testnet', (t) => {
+  t.plan(1)
+  fetch('https://id.parity.io:8443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
+})
+
+test('email verification – mainnet', (t) => {
+  t.plan(1)
+  fetch('https://email-verification.parity.io:18443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
+})
+
+test('email verification – testnet', (t) => {
+  t.plan(1)
+  fetch('https://email-verification.parity.io:28443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
+})
+
+test('sms verification – mainnet', (t) => {
+  t.plan(1)
+  fetch('https://sms-verification.parity.io:443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
+})
+
+test('sms verification – testnet', (t) => {
+  t.plan(1)
+  fetch('https://sms-verification.parity.io:8443/health')
+  .then((res) => {
+    t.ok(t.ok, 'service reported as non-healthy')
+  })
+  .catch((err) => {
+    t.error(err)
+  })
 })
