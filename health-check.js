45a5549f530dc46d7af15042d272a3ee73e3ee7c
Jannis R <mail@jannisr.de>
2017-01-23 17:28:16 +0100

    init

---
+++ health-check.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const test = require('tape')
+const fetch = require('node-fetch')
+
+test('todo', (t) => {
+  t.plan(1)
+  t.pass('todo')
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+# [Parity](https://ethcore.io/parity.html) services health check
+
+Checks on CI if all Parity services are running & ok.
